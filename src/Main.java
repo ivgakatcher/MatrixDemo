3c8efb44c9e7aa6c9c03f5246de6290dcf65f6f3
alkaz07 <alkaz07@yandex.ru>
2024-04-03 17:03:01 +0300

    +

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         int kStolb = 7;
         matrix = new int[kStrok][kStolb];
         printMatr(matrix);
+
+        matrix = generateRandomMatrix(5, 5);
+        printMatr(matrix);
     }
 
     public static void printMatr(int[][] matr){
@@ -12,7 +15,8 @@ public class Main {
            // int[] stroka = matr[i];
            // printArray("строка матрицы ", stroka);
             for (int j = 0; j <  matr[i].length; j++) {
-                System.out.print( matr[i][j]+" ");
+                //System.out.print( matr[i][j]+" ");
+                System.out.printf("%4d", matr[i][j]);   //форматируем для красивых ровных столбцов
             }
             System.out.println();
         }
@@ -22,7 +26,18 @@ public class Main {
         System.out.println(message +" "+ massiv.length + " чисел");
         for (int i = 0; i < massiv.length; i++) {
             System.out.print(massiv[i]+" ");
+
         }
         System.out.println();
+    }
+
+    public static int[][] generateRandomMatrix(int kRow, int kCol){
+        int[][] matrix=new int[kRow][kCol];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j <  matrix[i].length; j++) {
+                matrix[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        return matrix;
     }
 }
